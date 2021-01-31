66a25cd62eeca93ab661264eafdba4bbf16ac242
Kryxzael <kasper.ramskjell98@gmail.com>
2021-01-31 14:30:56 +0100

    Make mom end game :))

--- Assets/LKatS/Scripts/CallReponse/Mom.cs
+++ Assets/LKatS/Scripts/CallReponse/Mom.cs
@@ -1,8 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Mom : MonoBehaviour
 {
     public AudioSource Response;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerMovement>() != null)
+        {
+            SceneManager.LoadScene("Menu");
+        }
+    }
 }
